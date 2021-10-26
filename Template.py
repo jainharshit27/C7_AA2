f2b96fddc9f2373c8cf3448cc62d4aabd9c81af2
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-10-26 18:07:08 +0530

    Create Template.py

---
+++ Template.py
@@ -0,0 +1,13 @@
+# WHILE LOOP
+import random
+ring = random.randint(1,18)
+print(ring)
+
+i = 1
+while #Condition here:
+    if #compare ring and i:
+        print(i)
+        break
+    else:
+        print(i)
+        # Increase i
